7ef701971cb459a10310dd616eb8203375eb95a8
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-07-05 18:12:56 -0400

    added in new function that created more counters depending onthe users

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-import { useState } from 'react';
+import Counter from '../components/Counter';
 
 function Home() {
-  // first count is state value varable and will be what we display
-  // setCount is the updater function that will change our state varaiable
-  // the zero in useState() is where we set our state varabible to a deafult value if this is not definded here then it will remain undefinded.
-  const [count, setCount] = useState(0);
+  const counters = [{ title: 'Counter 1' }, { title: 'Counter 2' }, { title: 'Counter 3' }];
 
-  const handleClick = () => {
-    // this is basically an annoymous functinion that is taking in the setCount function and updating it here instead
-    // this is also where we can create a switch statement that handles all of the button functions
-    setCount((prevState) => prevState + 1);
-  };
-
-  // then return JSX
   return (
     <>
-      <h1>Counter</h1> {/* this is just the title */}
-      <h2>{count}</h2> {/* this is the count value set at 0 */}
-      <button type="button" onClick={handleClick}>Increment</button> {/* the handClick function is being called when the onClick is triggered from the button   */}
-      <button type="button" onClick={() => setCount((prevState) => prevState - 1)}>Decrement</button>
-      <button type="button" onClick={() => setCount(0)}>Reset</button>
+      {counters.map((counter) => (
+        <Counter key={counter.title} title={counter.title} />
+      ))}
     </>
   );
 }
